e0c18a7471b929e0be08aa9f14e13059e890ce86
CAIO SANTANA <cfs.plus.s8@gmail.com>
2020-12-15 14:57:53 -0300

    Sync

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import sys, os, threading
+import sys, os, threading, subprocess
 
 def launcher(target, args):
     thread = threading.Thread(target=target, args=args)
@@ -10,10 +10,15 @@ def main():
     path = os.path.dirname(os.path.abspath(sys.argv[0]))
     print(f'Initialize build on base path {path}...\n')
     
-    launcher(os.system, (f'dotnet build {path}/Services',))
-    launcher(os.system, (f'dotnet run --project {path}/Services/Services/Services.csproj',))
-
     os.chdir(f'{path}/Client')
-    launcher(os.system, ('yarn build | yarn start',))
+    os.system('yarn install')
+    os.system('yarn build')
+    os.system('yarn start')
+
+    os.chdir(f'{path}/Services')
+    launcher(os.system, (f'dotnet build',))
+
+    os.chdir(f'{path}/Services/Services')
+    launcher(os.system, (f'dotnet run --project Services.csproj',))
 
 main()
